562295a3788cfc7b17ba5bbbf2d3ae87a0489e0f
Simon Massey <simon@simonmassey.org>
2025-07-27 22:10:27 +0100

    step1

---
+++ json-compatibility-suite/src/test/java/jdk/sandbox/testsuite/PlaceholderTest.java
@@ -0,0 +1,11 @@
+package jdk.sandbox.testsuite;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PlaceholderTest {
+    @Test
+    void moduleExists() {
+        assertThat(true).isTrue();
+    }
+}
